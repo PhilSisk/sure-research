bd6541d81a470cd3f50a42df02ad61385107a2e8
Phil Sisk <psisk@umich.edu>
2018-08-21 19:48:06 -0400

    added untracked files

---
+++ phil_catkin_ws/build/rctestpkg/CMakeFiles/IMU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/IMU.dir/src/IMU.cpp.o"
+  "/home/ubuntu/phil_catkin_ws/devel/lib/rctestpkg/IMU.pdb"
+  "/home/ubuntu/phil_catkin_ws/devel/lib/rctestpkg/IMU"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/IMU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
